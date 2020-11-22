cd85081e9d3e34aa845c4479151ab29d3aa9971a
Clément van Staen <clement.vanstaen@gmail.com>
2020-11-22 13:27:58 +0100

    change index and port 5001

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 require("dotenv/config");
 
 // Init Express
