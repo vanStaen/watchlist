65edd676021df78d90d2bfdaad764a18ed455005
Clément van Staen <clement.vanstaen@gmail.com>
2020-12-02 22:04:13 +0100

    cords update with DEL

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 // Allow cross orign request
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, DEL, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 // Set up for React
 app.use(express.static(path.join(__dirname, "build")));
-app.get('/', (req,res) => {res.sendFile(path.join(__dirname,"build", "index.html"));});
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, "build", "index.html")); });
 
 // Router to Watchlist API
 app.use("/watchlist", require("./api/watchlist"));
