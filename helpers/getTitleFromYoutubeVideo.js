4a88271055a0522e882ffe2d8d89596e9414c768
Clément van Staen <clement.vanstaen@gmail.com>
2020-12-02 18:05:20 +0100

    deploy

--- helpers/getTitleFromYoutubeVideo.js
+++ helpers/getTitleFromYoutubeVideo.js
@@ -5,7 +5,6 @@ async function getTitleFromYoutubeVideo(videoID) {
 
     const googleApiKey = process.env.GOOGLE_API_KEY;
     const googleApiUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + videoID + "&key=" + googleApiKey;
-
     const resTitle = await axios.get(googleApiUrl)
         .then(response => {
             title = response.data.items[0].snippet.title;
