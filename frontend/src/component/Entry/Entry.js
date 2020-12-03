e8f8fd5a11312ee3f92f35c87a35d1e805280b1b
vanStaen <clement.vanstaen@gmail.com>
2020-12-03 12:27:48 +0100

    general styling

--- frontend/src/component/Entry/Entry.js
+++ frontend/src/component/Entry/Entry.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { YoutubeOutlined } from '@ant-design/icons';
 import Tags from './Tags/Tags'
 import DeleteButton from './Actions/DeleteButton'
+import RateThisVideo from './Actions/RateThisVideo'
 import './Entry.css'
 
 
@@ -9,12 +10,16 @@ const Entry = props => {
 
   const handlerFlipDiv = () => {
     const id = 'inner' + props.entry.id;
+    const img = 'img' + props.entry.id;
     document.getElementById(id).style.transform = "translateX(-100%) rotateY(-180deg)";
+    setTimeout(function () { document.getElementById(img).style.display = "none"; }, 500);
+
   }
 
   const handlerFlipDivBack = () => {
     const id = 'inner' + props.entry.id;
-    console.log('HERE');
+    const img = 'img' + props.entry.id;
+    document.getElementById(img).style.display = "block";
     document.getElementById(id).style.transform = "rotateY(0deg)";
   }
 
@@ -31,6 +36,7 @@ const Entry = props => {
         <div className='entry-card-front' onClick={handlerFlipDiv}>
           <img
             className='entry-card__img'
+            id={'img' + props.entry.id}
             alt={props.entry.title}
             src={youtubeVideoID}
           />
@@ -50,6 +56,8 @@ const Entry = props => {
           </div>
 
           <div className='entry-card-back__actions'>
+            <RateThisVideo id={props.entry.id} />
+            &nbsp;&nbsp;&nbsp;&nbsp;
             <DeleteButton id={props.entry.id} />
           </div>
 
