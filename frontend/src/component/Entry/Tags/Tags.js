24777d76f83c22856d3289a1d869661ceb7102da
Clément van Staen <clement.vanstaen@gmail.com>
2020-12-04 11:27:21 +0100

    wip axios patch tags

--- frontend/src/component/Entry/Tags/Tags.js
+++ frontend/src/component/Entry/Tags/Tags.js
@@ -1,5 +1,6 @@
 import { React, useState } from 'react';
 import { Tag, Input } from 'antd';
+import axios from 'axios';
 import { PlusOutlined } from '@ant-design/icons';
 
 import './Tags.css'
@@ -12,15 +13,37 @@ const Tags = props => {
     const [inputValue, setInputValue] = useState('');
 
     const patchTagsInDB = (tags) => {
+        async function patchEntry(tags) {
+            const response = await axios({
+                url: 'https://watchlist-cvs.herokuapp.com/watchlist/' + props.id,
+                method: 'PATCH',
+                data: { 'tags': tags }
+            });
+            if ((response.status !== 200) & (response.status !== 201)) {
+                throw new Error("Error!");
+            }
+            const patchResult = await response.data;
+            return patchResult;
+        }
+        // fetch Entries
+        patchEntry(tags).then((resData) => {
+            const patchResult = resData;
+            console.log("Sucess", patchResult);
+        }
+        ).catch(error => {
+            console.log("error", error.message);
+        });
+
         console.log('id', props.id);
         console.log('tags', tags);
     }
 
+
     const handlerDeleteTag = (deleteTagIndex) => {
         let oldTags = tags;
         let deletedTags = oldTags.splice(deleteTagIndex, 1);
+        patchTagsInDB(tags);
         setTags(oldTags);
-        console.log(oldTags);
     }
 
     const handleEditInputConfirm = e => {
@@ -142,141 +165,3 @@ const Tags = props => {
 
 export default Tags
 
-
-
-/*
-class EditableTagGroup extends React.Component {
-
-    state = {
-        tags: ['Unremovable', 'Tag 2', 'Tag 3'],
-        inputVisible: false,
-        inputValue: '',
-        editInputIndex: -1,
-        editInputValue: '',
-    };
-
-    handleClose = removedTag => {
-        const tags = this.state.tags.filter(tag => tag !== removedTag);
-        console.log(tags);
-        this.setState({ tags });
-    };
-
-    showInput = () => {
-        this.setState({ inputVisible: true }, () => this.input.focus());
-    };
-
-    handleInputChange = e => {
-        this.setState({ inputValue: e.target.value });
-    };
-
-    handleInputConfirm = () => {
-        const { inputValue } = this.state;
-        let { tags } = this.state;
-        if (inputValue && tags.indexOf(inputValue) === -1) {
-            tags = [...tags, inputValue];
-        }
-        console.log(tags);
-        this.setState({
-            tags,
-            inputVisible: false,
-            inputValue: '',
-        });
-    };
-
-    handleEditInputChange = e => {
-        this.setState({ editInputValue: e.target.value });
-    };
-
-    handleEditInputConfirm = () => {
-        this.setState(({ tags, editInputIndex, editInputValue }) => {
-            const newTags = [...tags];
-            newTags[editInputIndex] = editInputValue;
-
-            return {
-                tags: newTags,
-                editInputIndex: -1,
-                editInputValue: '',
-            };
-        });
-    };
-
-    saveInputRef = input => {
-        this.input = input;
-    };
-
-    saveEditInputRef = input => {
-        this.editInput = input;
-    };
-
-    render() {
-        return (
-            <>
-                {tags.map((tag, index) => {
-                    if (editInputIndex === index) {
-                        return (
-                            <Input
-                                ref={this.saveEditInputRef}
-                                key={tag}
-                                size="small"
-                                className="tag-input"
-                                value={editInputValue}
-                                onChange={this.handleEditInputChange}
-                                onBlur={this.handleEditInputConfirm}
-                                onPressEnter={this.handleEditInputConfirm}
-                            />
-                        );
-                    }
-
-                    const isLongTag = tag.length > 20;
-
-                    const tagElem = (
-                        <Tag
-                            className="edit-tag"
-                            key={tag}
-                            closable={index !== 0}
-                            onClose={() => this.handleClose(tag)}
-                        >
-                            <span
-                                onDoubleClick={e => {
-                                    if (index !== 0) {
-                                        this.setState({ editInputIndex: index, editInputValue: tag }, () => {
-                                            this.editInput.focus();
-                                        });
-                                        e.preventDefault();
-                                    }
-                                }}
-                            >
-                                {isLongTag ? `${tag.slice(0, 20)}...` : tag}
-                            </span>
-                        </Tag>
-                    );
-                    return isLongTag ? (
-                        <Tooltip title={tag} key={tag}>
-                            {tagElem}
-                        </Tooltip>
-                    ) : (
-                            tagElem
-                        );
-                })}
-                {inputVisible && (
-                    <Input
-                        ref={this.saveInputRef}
-                        type="text"
-                        size="small"
-                        className="tag-input"
-                        value={inputValue}
-                        onChange={this.handleInputChange}
-                        onBlur={this.handleInputConfirm}
-                        onPressEnter={this.handleInputConfirm}
-                    />
-                )}
-                {!inputVisible && (
-                    <Tag className="site-tag-plus" onClick={this.showInput}>
-                        <PlusOutlined /> New Tag
-                    </Tag>
-                )}
-            </>
-        );
-    }
-}
- */
