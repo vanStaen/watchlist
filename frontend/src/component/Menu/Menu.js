51fd7a435ce0f3f4dbccdf7477fbb26bd392b7a8
vanStaen <clement.vanstaen@gmail.com>
2020-12-12 23:46:08 +0100

    menu first version

--- frontend/src/component/Menu/Menu.js
+++ frontend/src/component/Menu/Menu.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import sortIcon from './sort.png'
-import filterIcon from './filter.png'
+import React from 'react';
+import sortIcon from './icons/sort.png';
+import filterIcon from './icons/filter.png';
+import addIcon from './icons/add.png';
+import loginIcon from './icons/login.png';
 import './Menu.css'
 
 const Menu = () => {
@@ -45,7 +47,7 @@ const Menu = () => {
                 src={sortIcon}
                 alt="Sort">
               </img>
-              <span className="link-text">Sorting</span>
+              <span className="link-text">Sort</span>
             </a>
           </li>
 
@@ -62,32 +64,26 @@ const Menu = () => {
 
           <li className="nav-item">
             <a href="#" className="nav-link">
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fad"
-                data-icon="space-shuttle"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 640 512"
-                className="svg-inline--fa fa-space-shuttle fa-w-20 fa-5x"
-              >
-                <g className="fa-group">
-                  <path
-                    fill="currentColor"
-                    d="M32 416c0 35.35 21.49 64 48 64h16V352H32zm154.54-232h280.13L376 168C243 140.59 222.45 51.22 128 34.65V160h18.34a45.62 45.62 0 0 1 40.2 24zM32 96v64h64V32H80c-26.51 0-48 28.65-48 64zm114.34 256H128v125.35C222.45 460.78 243 371.41 376 344l90.67-16H186.54a45.62 45.62 0 0 1-40.2 24z"
-                    className="fa-secondary"
-                  ></path>
-                  <path
-                    fill="currentColor"
-                    d="M592.6 208.24C559.73 192.84 515.78 184 472 184H186.54a45.62 45.62 0 0 0-40.2-24H32c-23.2 0-32 10-32 24v144c0 14 8.82 24 32 24h114.34a45.62 45.62 0 0 0 40.2-24H472c43.78 0 87.73-8.84 120.6-24.24C622.28 289.84 640 272 640 256s-17.72-33.84-47.4-47.76zM488 296a8 8 0 0 1-8-8v-64a8 8 0 0 1 8-8c31.91 0 31.94 80 0 80z"
-                    className="fa-primary"
-                  ></path>
-                </g>
-              </svg>
-              <span className="link-text">Shuttle</span>
+              <img
+                className="nav-icon svg-inline--fa fa-w-18 fa-9x"
+                src={addIcon}
+                alt="Sort">
+              </img>
+              <span className="link-text">Add</span>
             </a>
           </li>
+
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              <img
+                className="nav-icon svg-inline--fa fa-w-18 fa-9x"
+                src={loginIcon}
+                alt="Sort">
+              </img>
+              <span className="link-text">Login</span>
+            </a>
+          </li>
+
         </ul>
       </nav>
     </div >
